acafd792140f923c5b74ff3ddff147bfb324622e
Chris Vig <chris@invictus.so>
2020-09-22 17:01:36 -0500

    Updates for 0.2.0 release.

--- TacBoard/Source/Utility/VersionManager.swift
+++ TacBoard/Source/Utility/VersionManager.swift
@@ -13,6 +13,15 @@ class VersionManager {
 
     // MARK: Types
     
+    /// Enumeration of custom upgrade actions for various versions.
+    private enum UpgradeAction: String, CaseIterable {
+     
+        /// Upgrades for version 0.2.0.
+        case version0p2p0
+        
+    }
+    
+    /// Enumeration of settings maintained by the `VersionManager` class.
     private enum Setting: String {
      
         // MARK: Cases
@@ -28,6 +37,9 @@ class VersionManager {
         
         /// If set to `false`, the user has not yet viewed the release notes for this version.
         case userHasViewedReleaseNotesForThisVersion
+        
+        /// The list of completed upgrade actions.
+        case completedUpgradeActions
         
         // MARK: Properties
         
@@ -68,6 +80,9 @@ class VersionManager {
             // This is a new install. Don't pester the user about checking the release notes
             userHasViewedReleaseNotesForThisVersion = true
             
+            // Mark all upgrade actions as completed, since we don't need to do any of them
+            completedUpgradeActions = Set(UpgradeAction.allCases)
+            
         } else if (currentVersionMajor > previousVersionMajor) ||
                   (currentVersionMajor == previousVersionMajor && currentVersionMinor > previousVersionMinor) ||
                   (currentVersionMajor == previousVersionMajor && currentVersionMinor == previousVersionMinor && currentVersionRevision > previousVersionRevision) {
@@ -76,6 +91,9 @@ class VersionManager {
             userHasViewedReleaseNotesForThisVersion = false
             
         }
+        
+        // Perform any required update actions
+        performUpgradeActions()
         
         // Now update the current version
         defaults.set(currentVersionMajor, forKey: Setting.previousVersionMajor.key)
@@ -90,6 +108,45 @@ class VersionManager {
     var userHasViewedReleaseNotesForThisVersion: Bool {
         get { return defaults.bool(forKey: Setting.userHasViewedReleaseNotesForThisVersion.key) }
         set { defaults.set(newValue, forKey: Setting.userHasViewedReleaseNotesForThisVersion.key) }
+    }
+    
+    // MARK: Private Utility
+    
+    /// Performs the upgrade actions for the specified
+    private func performUpgradeActions() {
+        
+        // Get the current set of upgrade actions which are already complete
+        var completedUpgradeActions = self.completedUpgradeActions
+        
+        // Run through each upgrade action in order, skipping any that are complete
+        for upgradeAction in UpgradeAction.allCases {
+            if completedUpgradeActions.contains(upgradeAction) { continue }
+            switch upgradeAction {
+            
+            case .version0p2p0:
+                // Auto-enable Ka-50 since we added content for it
+                guard let ka50 = AircraftModule.for(key: "Ka-50") else { continue }
+                SettingsManager.shared.enabledAircraftModules.value.insert(ka50)
+            
+            }
+            completedUpgradeActions.insert(upgradeAction)
+        }
+        
+        // Update the set of completed upgrade actions
+        self.completedUpgradeActions = completedUpgradeActions
+        
+    }
+    
+    /// The set of completed upgrade actions.
+    private var completedUpgradeActions: Set<UpgradeAction> {
+        get {
+            let array = (defaults.object(forKey: Setting.completedUpgradeActions.key) as? [String]) ?? []
+            return Set(array.compactMap { UpgradeAction(rawValue: $0) })
+        }
+        set {
+            let array = newValue.map { $0.rawValue }
+            defaults.set(array, forKey: Setting.completedUpgradeActions.key)
+        }
     }
     
 }
